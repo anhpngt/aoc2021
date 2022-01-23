9259fcaff6aa0b5884d974a2f85539191621dba4
Tuan Anh <anh.pngt@gmail.com>
2022-01-23 18:41:23 +0700

    Add day 2.1

---
+++ src/2.ts
@@ -0,0 +1,32 @@
+import { readFileSync } from "fs";
+
+const inputFileName = "inputs/2.txt"
+const fileContent = readFileSync(inputFileName, 'utf-8');
+const lines = fileContent.split('\n').map(line => parseCommand(line));
+
+type Command = [string, number]
+
+function parseCommand(line: string): Command {
+    const [action, value] = line.split(' ');
+    return [action, Number(value)];
+}
+
+export function solveDay2_1() {
+    let pos = 0;
+    let depth = 0;
+    for (const [action, value] of lines) {
+        switch (action) {
+            case 'down':
+                depth += value;
+                break;
+            case 'up':
+                depth -= value;
+                break;
+            case 'forward':
+                pos += value;
+                break;
+        }
+    }
+
+    console.log(`final position: ${pos}, depth: ${depth} => ${pos * depth}`);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { solveDay1_1, solveDay1_2 } from './1';
+import { solveDay2_1 } from './2';
 
 const targetDay = Number(process.argv[2]);
+console.log(`Solving day ${targetDay}`)
 switch (targetDay) {
     case 1:
         solveDay1_1();
         solveDay1_2();
+        break;
+    case 2:
+        solveDay2_1();
         break;
     default:
         console.log(`Day ${targetDay} not implemented yet`);
