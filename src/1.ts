cbea50f19e22fbf43e5cb33be2ea0a9534470bec
Tuan Anh <anh.pngt@gmail.com>
2022-01-23 17:56:07 +0700

    Add day 1.2

--- src/1.ts
+++ src/1.ts
@@ -1,10 +1,10 @@
 import { readFileSync } from "fs";
 
-export function solveDay1_1() {
-    const inputFileName = "inputs/1.txt"
-    const fileContent = readFileSync(inputFileName, 'utf-8');
-    const lines = fileContent.split('\n').map(line => Number(line));
+const inputFileName = "inputs/1.txt"
+const fileContent = readFileSync(inputFileName, 'utf-8');
+const lines = fileContent.split('\n').map(line => Number(line));
 
+export function solveDay1_1() {
     let a = lines[0]
     let count = 0
     for (let line of lines) {
@@ -12,6 +12,20 @@ export function solveDay1_1() {
             count++
         }
         a = line
+    }
+
+    console.log(count)
+}
+
+export function solveDay1_2() {
+    let a = lines.slice(0, 3).reduce((a, b) => a + b)
+    let count = 0
+    for (let i = 2, i_end = lines.length - 1; i < i_end; i++) {
+        let b = lines.slice(i - 1, i + 2).reduce((a, b) => a + b)
+        if (b > a) {
+            count++
+        }
+        a = b
     }
 
     console.log(count)
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import {solveDay1_1} from './1';
+import { solveDay1_1, solveDay1_2 } from './1';
 
 const targetDay = Number(process.argv[2]);
 switch (targetDay) {
     case 1:
         solveDay1_1();
+        solveDay1_2();
         break;
     default:
         console.log(`Day ${targetDay} not implemented yet`);
