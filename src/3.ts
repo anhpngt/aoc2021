5d82121c636eb6784eedae352fba68ce98ceec17
Tuan Anh <anh.pngt@gmail.com>
2022-01-25 21:09:10 +0700

    Add day 3.2

--- src/3.ts
+++ src/3.ts
@@ -32,3 +32,73 @@ export function solveDay3_1() {
     const epsilon = parseInt(epsilonBits.join(""), 2)
     console.log(`gamma: ${gamma}, epsilon: ${epsilon} => ${gamma * epsilon}`)
 }
+
+export function solveDay3_2() {
+    const oxygen = findOxygenRating()
+    const co2 = findCO2Rating()
+    console.log(`Oxygen: ${oxygen}, CO2: ${co2} => ${oxygen * co2}`)
+}
+
+function findOxygenRating(): number {
+    let mask = new Array<boolean>(H).fill(true)
+    let validCount = H
+
+    for (let w = 0; w < W; w++) {
+        let count = 0
+        for (let h = 0; h < H; h++) {
+            if (mask[h] && lines[h][w] === '1') {
+                count++
+            }
+        }
+        let chosen = count >= validCount / 2 ? '1' : '0'
+        for (let h = 0; h < H; h++) {
+            if (mask[h] && lines[h][w] !== chosen) {
+                mask[h] = false
+                validCount--
+            }
+        }
+
+        if (validCount === 1) {
+            for (let h = 0; h < H; h++) {
+                if (mask[h]) {
+                    return parseInt(lines[h], 2)
+                }
+            }
+        }
+
+    }
+
+    throw Error("No solution found")
+}
+
+function findCO2Rating(): number {
+    let mask = new Array<boolean>(H).fill(true)
+    let validCount = H
+
+    for (let w = 0; w < W; w++) {
+        let count = 0
+        for (let h = 0; h < H; h++) {
+            if (mask[h] && lines[h][w] === '1') {
+                count++
+            }
+        }
+        let chosen = count >= validCount / 2 ? '0' : '1'
+        for (let h = 0; h < H; h++) {
+            if (mask[h] && lines[h][w] !== chosen) {
+                mask[h] = false
+                validCount--
+            }
+        }
+
+        if (validCount === 1) {
+            for (let h = 0; h < H; h++) {
+                if (mask[h]) {
+                    return parseInt(lines[h], 2)
+                }
+            }
+        }
+
+    }
+
+    throw Error("No solution found")
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { solveDay1_1, solveDay1_2 } from './1';
 import { solveDay2_1, solveDay2_2 } from './2';
-import { solveDay3_1 } from './3';
+import { solveDay3_1, solveDay3_2 } from './3';
 
 const targetDay = Number(process.argv[2]);
 console.log(`Solving day ${targetDay}`)
@@ -15,6 +15,7 @@ switch (targetDay) {
         break;
     case 3:
         solveDay3_1();
+        solveDay3_2();
         break;
     default:
         console.log(`Day ${targetDay} not implemented yet`);
